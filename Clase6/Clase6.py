f62b90c191c445210967b8b88c1ec4dc7ba677d9
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-04-04 09:37:20 +0200

    Añadir clase 6 01/04

---
+++ Clase6/Clase6.py
@@ -0,0 +1,32 @@
+
+
+#Crear una lista con los números del 1 al 5
+# lista = [1, 2, 3, 4, 5]
+#
+# #Modificar el tercer dato por 10
+# lista[2] = 10
+# print(lista)
+# #Agregar el número 7 al final utilizando append().
+# lista.append(7)
+# print(lista)
+
+#Actividad 2:
+#Crear una tupla con los números 1,2,3.
+
+tupla = (1, 2, 3)
+print(tupla)
+
+#Las tuplas son inmutables no se pueden modificar
+#salvo que se modifique directamente la tupla
+
+
+#Actividad 3.
+# Crear una matriz 3x3 con números consecutivos
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matriz[1][2])
+
+#Modificar el elemento de la segunda fila, tercera columna
+matriz[1][2] = 40
+print(matriz)
+
